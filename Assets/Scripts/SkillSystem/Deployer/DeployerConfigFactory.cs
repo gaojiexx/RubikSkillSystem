ccb584771528a5da2a97e775f79bf3e6449cdc91
gaojiexx <409881150@qq.com>
2024-04-12 07:26:51 +0800

    修改文件路径

---
+++ Assets/Scripts/SkillSystem/Deployer/DeployerConfigFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rubik.SkillSystem
+{
+    /// <summary>
+    /// 释放器工厂
+    /// </summary>
+    public class DeployerConfigFactory
+    {
+        private static Dictionary<int,string> SkillImpactTypeDict = new Dictionary<int, string>();
+        
+        public static void Init()
+        {
+            
+        }
+        
+        
+        // public static ISkillImpact[] CreateSkillImpacts(List<SkillImpactType> skillImpactTypes)
+        // {
+        //     List<ISkillImpact> skillImpacts = new List<ISkillImpact>();
+        //     foreach (var skillImpactType in skillImpactTypes)
+        //     {
+        //         ISkillImpact skillImpact = CreateSkillImpact<ISkillImpact>();
+        //         skillImpacts.Add(skillImpact);
+        //     }
+        //     
+        // }
+
+        private static T CreateSkillImpact<T>(string typeName) where T : class
+        {
+            var type = Type.GetType(typeName);
+            return Activator.CreateInstance(type) as T;
+        }
+    }
+}
---
+++ Assets/Scripts/SkillSystem/SkillImpact/SkillImpactAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Rubik.SkillSystem
+{
+    public class SkillImpactAttribute : Attribute
+    {
+        public SkillImpactType ImpactType { get; private set; }
+        public string Description { get; private set; }
+        public string TypeName { get; private set; }
+        
+        public SkillImpactAttribute(SkillImpactType impactType, string description, string typeName)
+        {
+            ImpactType = impactType;
+            Description = description;
+            TypeName = typeName;
+        }
+    }
+}
